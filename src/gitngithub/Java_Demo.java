61c39874727f5d3906dab2caa3537f4cac3165be
Komal <komaldhumale9333@gmail.com>
2023-05-11 19:46:14 +0530

    Message for new Repo

---
+++ src/gitngithub/Java_Demo.java
@@ -0,0 +1,11 @@
+package gitngithub;
+
+public class Java_Demo {
+
+	public static void main(String[] args)
+	{
+		System.out.println("We are learning Java basics");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ashis
+ *
+ */
+module Learning_Github {
+}
